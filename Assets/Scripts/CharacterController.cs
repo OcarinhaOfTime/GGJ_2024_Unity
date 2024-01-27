c62f762a61757fb9c516757b10fd3e2c53ce6e5b
Vagner Seibert <vagnerseibert@gmail.com>
2024-01-27 11:20:48 -0300

    Added movement to the player.

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour {
     ControlMap controlMap;
+    public float speed = 1.0f;
+
+    public Vector2 vel;
+    public Vector2 pos {
+        get => new Vector2(transform.localPosition.x, transform.localPosition.z);
+        set => transform.localPosition = new Vector3(value.x, transform.localPosition.y, value.y);
+    }
     private void Awake() {
         controlMap = new ControlMap();
         controlMap.Enable();
     }
 
     void Start() {
-        //controlMap.Player.Move. += ctx => Move(ctx.read)
+        controlMap.Player.Move.performed += 
+            ctx => Move(ctx.ReadValue<Vector2>());
+
+        controlMap.Player.Move.canceled +=
+            ctx => Move(ctx.ReadValue<Vector2>());
     }
 
     void Move(Vector2 m) {
+        vel = m;
+    }
 
+    private void FixedUpdate() {
+        pos += Time.deltaTime * speed * vel;
     }
 }
