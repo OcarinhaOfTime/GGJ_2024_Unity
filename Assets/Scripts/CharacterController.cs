ab9df923b2c1d51b3ce093e04969dc7ab98d52c0
OcarinhaOfTime <vagnerseibert@gmail.com>
2024-01-27 10:49:40 -0300

    Added playerController

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -3,5 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour {
-    
+    ControlMap controlMap;
+    private void Awake() {
+        controlMap = new ControlMap();
+        controlMap.Enable();
+    }
+
+    void Start() {
+
+    }
 }
