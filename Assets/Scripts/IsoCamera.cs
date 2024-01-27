19509e0301ac2cedd9d348669fda0517a087783e
Gabs-1 <gabriela.vitoria1202@gmail.com>
2024-01-27 13:21:22 -0300

    implementada câmera em 3a pessoa.
    implementada câmera em 3a pessoa.

--- Assets/Scripts/IsoCamera.cs
+++ Assets/Scripts/IsoCamera.cs
@@ -1,7 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class IsoCamera : MonoBehaviour {
-    
+public class IsoCamera : MonoBehaviour
+{
+    public Transform player;
+    public float smoothSpeed = 10f;
+    public Vector3 offset;
+
+    public float minX = -5f;
+    public float maxX = 5f;
+    private void Update()
+    {
+        Vector3 desiredPosition = player.position + offset;
+        //Vector3 smoothedPosition = Vector3.Lerp(Transform.position, desiredPosition, smoothSpeed);
+
+        //smoothedPosition.x = Mathf.Clamp(smoothedPosition.x, minX, maxX);
+
+        transform.position = desiredPosition;
+    }
 }
+   
